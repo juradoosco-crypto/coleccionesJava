0a4479645155854d2e2732c145fa8b0a20dd40b2
Alumno1 <juradoosco@gmail.com>
2026-03-23 12:06:45 +0100

    versión 1 eliza

--- src/ejHashMap/eje04/Ej04.java
+++ src/ejHashMap/eje04/Ej04.java
@@ -1,8 +1,42 @@
 package ejHashMap.eje04;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Ej04 {
 	public static void main(String[] args) {
+		var map = new HashMap <String,String>();
+		map.put("HOLA","Hola, ¿qué tal?");
+		map.put("ENCANTADO","Encantado de conocerte yo también");
+		map.put("ADIÓS","Adiós, espero volverte a ver pronto");
+		map.put("HORA","Lo siento, no llevo reloj");
+		map.put("NOMBRE","Mi nombre es Eliza");
 		
+		String msgDefault = "Lo siento, no te comprendo";
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Bienvenido al sistema Eliza 0.0:");
+		String frase;
+		boolean fin=false;
+		do {
+			frase = sc.nextLine();
+			boolean encontrada = false;
+			for (String clave: map.keySet()) {
+				if (frase.toUpperCase().contains(clave)) {
+					encontrada = true;
+					System.out.println(">"+map.get(clave));
+					if (frase.toUpperCase().contains("ADIOS")) {
+						fin = true;
+					}
+					break;
+				} 
+			}
+			if (!encontrada) {
+				System.out.println(msgDefault);
+			}
+			
+		} while (!fin);
+		System.out.println("Muchas gracias por conectarse");
 	}
 
 }
